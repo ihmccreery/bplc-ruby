d4dfe7af564501985cbbebb85560383e7c4ddc21
Isaac Hollander McCreery <ihmccreery@gmail.com>
2014-03-30 12:24:53 -0400

    WIP specs for order of operations

--- spec/parser_spec.rb
+++ spec/parser_spec.rb
@@ -19,6 +19,23 @@ describe Parser do
     end
   end
 
+  describe "building AssignmentExps" do
+    it "properly forms AssignmentExps"
+    it "chains AssignmentExps"
+    it "raises SyntaxErrors on bad lhss"
+  end
+
+  describe "order of operations" do
+    it "properly nests AssignmentExps"
+    it "properly nests RelExps"
+    it "properly nests AddExps and MulExps"
+    it "properly nests NegExps"
+    it "properly nests VarExps"
+    it "raises errors on improperly nested variable expressions"
+    it "properly nests LitExps"
+    it "properly nests parenthesized Exps"
+  end
+
   it "parses ex1.bpl properly" do
     expect(Parser.new(Scanner.new(File.new(EX1_FNAME))).parse).to be_a Ast
   end
