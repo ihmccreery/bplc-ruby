6d67debebf8bfd59b6c4375db41e15536e3d562a
Isaac Hollander McCreery <ihmccreery@gmail.com>
2014-04-07 09:31:37 -0400

    use :string instead of :str for string type, add VarExp types (not yet FunCallExps, though)

--- lib/type_checker.rb
+++ lib/type_checker.rb
@@ -11,7 +11,9 @@ class TypeChecker
   private
 
   def r(ast)
-    if ast.is_a? LitExp
+    if ast.is_a? VarExp
+      r_var_exp(ast)
+    elsif ast.is_a? LitExp
       r_lit_exp(ast)
     end
     ast.children.each do |c|
@@ -19,9 +21,68 @@ class TypeChecker
     end
   end
 
+  # @param ast [VarExp]
+  def r_var_exp(ast)
+    if ast.declaration.is_a? SimpleDeclaration
+      r_var_exp_simple_declaration(ast)
+    elsif ast.declaration.is_a? PointerDeclaration
+      r_var_exp_pointer_declaration(ast)
+    elsif ast.declaration.is_a? ArrayDeclaration
+      r_var_exp_array_declaration(ast)
+    else # ast.declaration.is_a? FunctionDeclaration
+      r_var_exp_function_declaration(ast)
+    end
+  end
+
+  def r_var_exp_simple_declaration(ast)
+    type_specifier = ast.declaration.type_specifier
+    if ast.is_a? SimpleVarExp
+      ast.type = type_specifier
+    elsif ast.is_a? PointerVarExp
+      raise SyntaxError, "cannot dereference #{type_specifier}"
+    elsif ast.is_a? ArrayVarExp
+      raise SyntaxError, "cannot index #{type_specifier}"
+    elsif ast.is_a? AddrVarExp
+      ast.type = ("pointer_" + ast.declaration.type_specifier.to_s).to_sym
+    elsif ast.is_a? AddrArrayVarExp
+      raise SyntaxError, "cannot index #{type_specifier}"
+    end
+  end
+
+  def r_var_exp_pointer_declaration(ast)
+    type_specifier = ast.declaration.type_specifier
+    if ast.is_a? SimpleVarExp
+      ast.type = ("pointer_" + ast.declaration.type_specifier.to_s).to_sym
+    elsif ast.is_a? PointerVarExp
+      ast.type = type_specifier
+    elsif ast.is_a? ArrayVarExp
+      raise SyntaxError, "cannot index pointer_#{type_specifier}"
+    elsif ast.is_a? AddrVarExp
+      raise SyntaxError, "cannot reference pointer_#{type_specifier}"
+    elsif ast.is_a? AddrArrayVarExp
+      raise SyntaxError, "cannot index pointer_#{type_specifier}"
+    end
+  end
+
+  def r_var_exp_array_declaration(ast)
+    type_specifier = ast.declaration.type_specifier
+    if ast.is_a? SimpleVarExp
+      ast.type = ("array_" + ast.declaration.type_specifier.to_s).to_sym
+    elsif ast.is_a? PointerVarExp
+      raise SyntaxError, "cannot dereference array_#{type_specifier}"
+    elsif ast.is_a? ArrayVarExp
+      ast.type = type_specifier
+    elsif ast.is_a? AddrVarExp
+      raise SyntaxError, "cannot reference array_#{type_specifier}"
+    elsif ast.is_a? AddrArrayVarExp
+      ast.type = ("pointer_" + ast.declaration.type_specifier.to_s).to_sym
+    end
+  end
+
+  # @param ast [LitExp]
   def r_lit_exp(ast)
     if ast.is_a? StrLitExp
-      ast.type = :str
+      ast.type = :string
     else
       ast.type = :int
     end
--- spec/type_checker_spec.rb
+++ spec/type_checker_spec.rb
@@ -39,6 +39,52 @@ describe TypeChecker do
     # VarExps #
     ###########
 
+    it "assigns SimpleDeclarations the correct type" do
+      a = type_check('int x; string y; void main(void) { x; y; &x; &y; }')
+
+      body = a.declarations[2].body
+      expect(body.stmts[0].exp.type).to eq(:int)
+      expect(body.stmts[1].exp.type).to eq(:string)
+      expect(body.stmts[2].exp.type).to eq(:pointer_int)
+      expect(body.stmts[3].exp.type).to eq(:pointer_string)
+    end
+
+    it "raises a SyntaxError if bad operators are used on SimpleDeclarations" do
+      expect{type_check('int x; void main(void) { *x; }')}.to raise_error(SyntaxError, "cannot dereference int")
+      expect{type_check('int x; void main(void) { x[1]; }')}.to raise_error(SyntaxError, "cannot index int")
+      expect{type_check('int x; void main(void) { &x[1]; }')}.to raise_error(SyntaxError, "cannot index int")
+    end
+
+    it "assigns PointerDeclarations the correct type" do
+      a = type_check('int *x; void main(void) { x; *x; }')
+
+      body = a.declarations[1].body
+      expect(body.stmts[0].exp.type).to eq(:pointer_int)
+      expect(body.stmts[1].exp.type).to eq(:int)
+    end
+
+    it "raises a SyntaxError if bad operators are used on PointerDeclarations" do
+      expect{type_check('int *x; void main(void) { &x; }')}.to raise_error(SyntaxError, "cannot reference pointer_int")
+      expect{type_check('int *x; void main(void) { x[1]; }')}.to raise_error(SyntaxError, "cannot index pointer_int")
+      expect{type_check('int *x; void main(void) { &x[1]; }')}.to raise_error(SyntaxError, "cannot index pointer_int")
+    end
+
+    it "assigns ArrayDeclarations the correct type" do
+      a = type_check('int x[2]; void main(void) { x; x[1]; &x[1]; }')
+
+      body = a.declarations[1].body
+      expect(body.stmts[0].exp.type).to eq(:array_int)
+      expect(body.stmts[1].exp.type).to eq(:int)
+      expect(body.stmts[2].exp.type).to eq(:pointer_int)
+    end
+
+    it "raises a SyntaxError if bad operators are used on ArrayDeclarations" do
+      expect{type_check('int x[2]; void main(void) { *x; }')}.to raise_error(SyntaxError, "cannot dereference array_int")
+      expect{type_check('int x[2]; void main(void) { &x; }')}.to raise_error(SyntaxError, "cannot reference array_int")
+    end
+
+    # # FunCallExp
+
     ###########
     # LitExps #
     ###########
@@ -48,7 +94,7 @@ describe TypeChecker do
 
       body = a.declarations[0].body
       expect(body.stmts[0].exp.type).to eq(:int)
-      expect(body.stmts[1].exp.type).to eq(:str)
+      expect(body.stmts[1].exp.type).to eq(:string)
       expect(body.stmts[2].exp.type).to eq(:int)
     end
   end
